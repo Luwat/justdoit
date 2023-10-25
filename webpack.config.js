f19c7a849611ef7715fbb157ca0fe35529c93eac
luwat <oluwatobioreniyi@gmail.com>
2023-10-25 13:04:13 +0100

    Add html-webpack-plugin and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Just Do It!'
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
 };
